a4cea90a78767cc68bc8b31d80b27caeb0b34e6c
sinthi23 <tanhaislamsinthi2003@gmail.com>
2026-01-09 18:59:35 +0600

    some modification

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,11 +1,7 @@
-import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
-
 plugins {
     id("com.android.application")
     id("com.google.gms.google-services")
 }
-
-val googleApiKey: String = gradleLocalProperties(rootDir, providers).getProperty("GOOGLE_API_KEY") ?: ""
 
 android {
     namespace = "com.example.mealrecmmenderandroid"
@@ -19,8 +15,6 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-
-        buildConfigField("String", "GOOGLE_API_KEY", "\"$googleApiKey\"")
     }
 
     buildTypes {
@@ -40,6 +34,7 @@ android {
 
     buildFeatures {
         viewBinding = true
+        dataBinding = true
         buildConfig = true
     }
 }
@@ -47,10 +42,10 @@ android {
 dependencies {
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.activity:activity:1.9.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.activity:activity:1.9.3")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.0")
 
-    // Firebase BOM
+    // Firebase
     implementation(platform("com.google.firebase:firebase-bom:33.7.0"))
     implementation("com.google.firebase:firebase-auth")
     implementation("com.google.firebase:firebase-database")
@@ -61,14 +56,12 @@ dependencies {
     implementation("androidx.recyclerview:recyclerview:1.3.2")
     implementation("androidx.cardview:cardview:1.0.0")
 
-    // Image Loading - Glide
+    // Image Loading
     implementation("com.github.bumptech.glide:glide:4.16.0")
     annotationProcessor("com.github.bumptech.glide:compiler:4.16.0")
 
-    // Credentials
-    implementation("androidx.credentials:credentials:1.3.0")
-    implementation("androidx.credentials:credentials-play-services-auth:1.3.0")
-    implementation("com.google.android.libraries.identity.googleid:googleid:1.1.1")
+    // Charts for Analytics
+    implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
 
     // Testing
     testImplementation("junit:junit:4.13.2")
--- build.gradle.kts
+++ build.gradle.kts
@@ -4,15 +4,16 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:8.3.2")  // Updated
+        classpath("com.android.tools.build:gradle:8.3.2")
         classpath("com.google.gms:google-services:4.4.2")
     }
 }
 
 plugins {
-    id("com.android.application") version "8.3.2" apply false  // Updated
+    id("com.android.application") version "8.3.2" apply false
+    id("com.google.gms.google-services") version "4.4.2" apply false
 }
 
 tasks.register("clean", Delete::class) {
-    delete(rootProject.buildDir)
+    delete(rootProject.layout.buildDirectory)
 }
